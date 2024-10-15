19977342dc5b290fde31f960872ae95c05c5d809
Saransh Gupta <saransh.gupta27@gmail.com>
2024-10-16 02:39:35 +0530

    feat: project init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Tailwind CSS
+
+ReactDOM.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
+  document.getElementById('root')
+);
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@chakra-ui/react/dist/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
